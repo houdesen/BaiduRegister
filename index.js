b2383e2f654dd94b9700926ad691370d7add1010
houdesen <zh06101019>
2020-03-17 15:32:35 +0800

    hudson

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ $(function () {
 
     //表单验证
     $("#btn").click(function(){
-        if (!validate('#username-input') || !validate('#phone-input') || !validate('#password-input'))
+        if (!validate('#username-input') || !validate('#phone-input') || !validate('#password-input') || validate('#verifyCode-input'))
             return;
     });
 
@@ -30,11 +30,15 @@ $(function () {
         if (!validate('#password-input'))
             return;
     })
+    $('#verifyCode-input').focusout(function(){
+        if (!validate('#verifyCode-input'))
+            return;
+    })
 
     function validate(field) {
         var $data = $(field);
         var $msg = $(field + '-validate');
-
+        
         if ($data.val() === '') {
             $msg.html('不能为空');
             return false;
@@ -43,8 +47,6 @@ $(function () {
             if (!(/^(?!\d+$)[a-zA-Z0-9-_\u4E00-\u9FA5]+$/.test($data.val()))) {
                 $msg.html('用户名仅支持中英文、数字和下划线、且不能为纯数字');
                 return false;
-            }else{
-                $msg.html('');
             }
 
         }
@@ -52,8 +54,6 @@ $(function () {
             if (!(/^((13[0-9])|(17[0-1,6-8])|(15[^4,\\D])|(18[0-9]))\d{8}$/.test($data.val()))) {
                 $msg.html('手机号码格式不正确');
                 return false;
-            } else{
-                $msg.html('');
             }
 
         }
@@ -61,11 +61,9 @@ $(function () {
             if (!(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[^]{8,16}$/.test($data.val()))) {
                 $msg.html('密码设置不符合要求');
                 return false;
-            }else{
-                $msg.html('');
             }
         }
-        $msg.html();
+        $msg.html('');
         return true;
     }
 
@@ -77,11 +75,11 @@ $(function () {
         var time = setInterval(function() {
             if (countdown == 0) {
                 $(".getVerifyCode").val("获取验证码");
-                $('#verifyCode-input-validate').css('display', 'block');
+                $('#verifyCode-input-validate').html('请求超时，请稍后再试');
                 countdown = 60;
                 clearInterval(time);
             } else {
-                $('#verifyCode-input-validate').css('display', 'none');
+                $('#verifyCode-input-validate').html();
                 countdown--;
                 $(".getVerifyCode").val("重新发送(" + countdown + ")");
             }
